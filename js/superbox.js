fe9aed0e327e7e58e3634ab23262970173cf8345
Adam Merrifield <macagp@gmail.com>
2013-04-24 17:34:06 -0400

    Initial heavy lifting on v3 rewrite.

--- js/superbox.js
+++ js/superbox.js
@@ -14,257 +14,112 @@
 	'use strict';
 
 	var pluginName = 'SuperBox',
-		pluginVersion = '2.1.1';
+		pluginVersion = '3.0.0';
 
 	$.fn.SuperBox = function(options) {
-
-		/**
-		 * OPTIONS
-		 */
-		var defaults = $.extend({
-			background : null,
-			border : null,
-			height : 400,
-			view : 'landscape',
-			xColor : null,
-			xShadow : 'none'
-		}, options);
 
 		/**
 		 * DECLARATIONS
 		 */
 		var sbImgBottom,
 			sbShowTop,
-			sbShow = $('<div class="superbox-show superbox-X"/>'),
+			sbShow = $('<div class="superbox-show"/>'),
 			sbImg = $('<img src="img/ajax-loader.gif" class="superbox-current-img"/>'),
 			sbClose = $('<a href="#" class="superbox-close"><i class="icon-cancel-circle"></i></a>'),
 			sbFloat = $('<div class="superbox-float"/>'),
-			sbList = this.find('>div');
+			sbList = this.find('>div'),
+			sbList8 = this.find('>div:nth-child(8n)'),
+			sbList6 = this.find('>div:nth-child(6n)'),
+			sbList4 = this.find('>div:nth-child(4n)'),
+			sbList2 = this.find('>div:nth-child(2n)');
 
 		/**
 		 * METHODS
 		 */
 
 		/**
-		 * bodyHeight
+		 * setLastClass
 		 * 
-		 * In order to circumvent a pages desire to collapse between slides
-		 * we temporarily set the html height, then unset it when we're done.
+		 * Last thumbnail is determined based on window width.
 		 */
-		var bodyHeight = (function(action){
-			if (action === 'set') {
-				$('html').css('height',$('html').outerHeight(true));
-			} else if (action == 'unset') {
-				$('html').css('height','');
-			}
-		});
-
-		/**
-		 * closeUp
-		 * 
-		 * Closes any open superbox-show
-		 * Accepts element (selector) parameter.
-		 */
-		var closeUp = (function(elem){
-
-			var selection = elem.next('.superbox-O');
-
-			/*
-			 * Swap open and closed classes
-			 */
-			selection.removeClass('superbox-O').addClass('superbox-X');
-
-			/*
-			 * Is a superbox-show in the same row already open? No? Then...
-			 */
-			if (sbImgBottom != sbShowTop) {
-				selection
-					/*
-					 * Fade out image and close icon
-					 */
-					.find('img.superbox-current-img, a.superbox-close')
-						.animate({opacity:0},100)
-					.end()
-						/*
-						 * Slide up superbox-show
-						 */
-						.slideUp(function(){
-							sbShowTop = 0;
-							sbImgBottom = 0;
-							/*
-							 * unset the html height
-							 */
-							bodyHeight('unset');
-						});
+		var setLastClass = function(){
+			if ($(window).width() > 1024) {
+				sbList.removeClass('superbox-last');
+				sbList8.addClass('superbox-last');
+			} else if ($(window).width() > 767) {
+				sbList.removeClass('superbox-last');
+				sbList6.addClass('superbox-last');
+			} else if ($(window).width() > 485) {
+				sbList.removeClass('superbox-last');
+				sbList4.addClass('superbox-last');
 			} else {
-
-				/*
-				 * temporarily set the html height
-				 */
-				bodyHeight('set');
-
-				selection
-					/*
-					 * Fade out image
-					 */
-					.find('img.superbox-current-img')
-						.animate({opacity:0},100)
-					.end()
-						/*
-						 * Since a superbox-show in the same row is already open, no need to slide up and down
-						 */
-						.hide();
+				sbList.removeClass('superbox-last');
+				sbList2.addClass('superbox-last');
 			}
-		});
+		};
 
 		/**
-		 * openUp
+		 * createSuperboxShow
 		 * 
-		 * opens the clicked $(this).next() superbox-show
-		 * Accepts element (selector) parameter.
+		 * Dynamically create superbox-show and insert it after superbox-last
 		 */
-		var openUp = (function(elem, fullImg){
-
-			var selection = elem.next();
-
-			/*
-			 * Add source data to dynamically created full size image
-			 */
-			selection.find('img.superbox-current-img')
-				.attr('src',fullImg);
-
-			/*
-			 * Set width of superbox-show 
-			 */
-
-			var setWidth = (function(){
-				if ($(window).width() > 1024) {
-					if (selection.outerWidth(true) != elem.width()*8) {
-						selection.outerWidth(elem.width()*8);
+		var createSuperboxShow = function(elem){
+			var noSuperbox = !$('.superbox-show').length,
+				notLast = !elem.hasClass('superbox-last'),
+				notInRowA = !elem.nextAll('.superbox-last:first').next('.superbox-show').length,
+				notInRowB = !elem.next('.superbox-show').length,
+				setA = function(){
+					sbShow.append(sbImg).append(sbClose).insertAfter(elem.nextAll('.superbox-last:first'));
+				},
+				setB = function(){
+					sbShow.append(sbImg).append(sbClose).insertAfter(elem);
+				},
+				create = function(){
+					if (notLast === true && notInRowA === true) {
+						if (noSuperbox === true) {
+							setA();
+						} else {
+							$('.superbox-show').slideUp(function(){
+								setA();
+							});
+						}
+					} else if (notLast === false && notInRowB === true) {
+						if (noSuperbox === true) {
+							setA();
+						} else {
+							$('.superbox-show').slideUp(function(){
+								setB();
+							});
+						}
 					}
-				} else if ($(window).width() > 767) {
-					if (selection.outerWidth(true) != elem.width()*6) {
-						selection.outerWidth(elem.width()*6);
-					}
-				} else if ($(window).width() > 485) {
-					if (selection.outerWidth(true) != elem.width()*4) {
-						selection.outerWidth(elem.width()*4);
-					}
-				} else {
-					if (selection.outerWidth(true) != elem.width()*2) {
-						selection.outerWidth(elem.width()*2);
-					}
-				}
-			});
+				};
 
-			/*
-			 * Set height of superbox-show 
-			 */
+			create();
+		};
 
-			var setHeight = (function(){
-				var thisWidth = selection.outerWidth(true),
-					thisHeight = defaults.height + (16 * 3), /* 1.5em padding */
-					newHeight = thisHeight,
-					thisWindow = $(window).height() * 0.80,
-					thisView = defaults.view,
-					thisRatio = 0.6667;
+		/**
+		 * openSuperboxShow
+		 * 
+		 * opens superbox-show when present.
+		 */
+		var openSuperboxShow = function(){
+			$('.superbox-show').slideDown();
+		};
 
-				if (newHeight > thisWindow) {
-					newHeight = thisWindow;
-				}
-
-				if (thisView === 'landscape') {
-					if (thisWidth < newHeight / thisRatio) {
-						newHeight = thisWidth * thisRatio;
-					}
-				}
-
-				if (thisView === 'portrait') {
-					if (thisWidth < newHeight * thisRatio) {
-						newHeight = thisWidth / thisRatio;
-					}
-				}
-
-				if (thisView === 'square') {
-					if (thisWidth < newHeight) {
-						newHeight = thisWidth;
-					}
-				}
-
-				selection.outerHeight(newHeight);
-			});
-
-			setWidth();
-			setHeight();
+		/**
+		 * keepShowAfterLast
+		 * 
+		 * Move superbox-show to after superbox-last when window is resized
+		 */
+		var keepShowAfterLast = function(){
 			$(window).resize(function(){
-				setWidth();
-				setHeight();
+				if ($('.superbox-O').hasClass('superbox-last')) {
+					$('.superbox-show').insertAfter($('.superbox-O'));
+				} else {
+					$('.superbox-show').insertAfter($('.superbox-O').nextAll('.superbox-last:first'));
+				}
 			});
-
-			/*
-			 * Swap open and closed classes
-			 */
-			selection.removeClass('superbox-X').addClass('superbox-O');
-
-			/*
-			 * Is a superbox-show in the same row already open? No? Then...
-			 */
-			if (sbImgBottom != sbShowTop) {
-
-				/*
-				 * Slide down superbox-show
-				 */
-				selection.slideDown(function(){
-
-					/*
-					 * Set sdShowTop position to top of superbox-show
-					 */
-					sbShowTop = $(this).offset().top;
-
-					/*
-					 * Scroll so that superbox-show is vertically centered
-					 */
-					$('html,body').animate({
-							scrollTop: sbShowTop - (($(window).height() - $(this).outerHeight(true))/2)
-					}, 'fast');
-
-					/*
-					 * Reset sbImgBottom to current bottom position of clicked image
-					 */
-					sbImgBottom = $(this).prev().find('img').offset().top + $(this).prev().find('img').height();
-
-				})
-					/*
-					 * Fade in close-icon
-					 */
-					.find('a.superbox-close')
-						.animate({opacity:0.7},1000);
-
-			} else {
-
-				selection
-					/*
-					 * Since a superbox-show in the same row is already open, no need to slide up and down
-					 */
-					.show()
-					/*
-					 * set close icon opacity
-					 */
-					.find('a.superbox-close')
-						.css('opacity','0.7');
-
-				/*
-				 * unset the html height
-				 */
-				bodyHeight('unset');
-			}
-
-			/*
-			 * Fade in image and close-icon
-			 */
-			selection.find('img.superbox-current-img')
-				.animate({opacity:1},1000);
-		});
+		};
 
 		/**
 		 * IMPLEMENTATION
@@ -281,68 +136,17 @@
 		sbList.addClass('superbox-list');
 
 		/*
-		 * Create dynamic superbox content
+		 * Set superbox-last class
 		 */
-		sbShow.append(sbImg).append(sbClose).insertAfter(sbList);
+		setLastClass();
+		$(window).resize(function(){
+			setLastClass();
+		});
 
 		/*
 		 * Create final float
 		 */
 		sbFloat.appendTo(this);
-
-		/*
-		 * Test for and utilize defaults.background
-		 */
-		if (defaults.background !== null) {
-			sbList
-				.next()
-					.css({
-						'background-color': defaults.background
-					});
-		}
-
-		/*
-		 * Test for and utilize defaults.border
-		 */
-		if (defaults.border !== null) {
-			sbList
-				.next()
-					.find('img.superbox-current-img')
-						.css({
-							'border-color': defaults.border
-						});
-		}
-
-		/*
-		 * Test for and utilize defaults.xColor
-		 */
-		if (defaults.xColor !== null) {
-			sbList
-				.next()
-					.find('a.superbox-close')
-						.css({
-							'color': defaults.xColor
-						});
-		}
-
-		/*
-		 * Test for and utilize defaults.xShadow
-		 */
-		if (defaults.xShadow == 'emboss') {
-			sbList
-				.next()
-					.find('a.superbox-close')
-						.css({
-							'text-shadow': '0 1px 0 rgba(0,0,0,0.6), 0 -1px 0 rgba(250,250,250,0.2)'
-						});
-		} else if (defaults.xShadow == 'embed') {
-			sbList
-				.next()
-					.find('a.superbox-close')
-						.css({
-							'text-shadow': '0 -1px 0 rgba(0,0,0,0.4), 0 1px 0 rgba(250,250,250,0.5)'
-						});
-		}
 
 		/*
 		 * Iterate through each superbox-list
@@ -354,35 +158,50 @@
 			 */
 			$(this).on('click',function(){
 
+				/* pseudocode
+				if superbox-show does not exist
+					create superbox-show
+					set superbox-show image src to imageData
+					animate opening of superbox-show
+					reveal image
+					move superbox-show to center screen
+				else
+					if superbox-show img is same as this imageData
+						animate closing of superbox-show
+					else
+						set superbox-show image src to imageData
+						reveal image
+					endif
+				endif
+
+				set image data
+				reveal image
+				*/
+
 				/**
 				 * DECLARATIONS
 				 */
 				var imageData = $(this).find('img').data('img');
+
+				sbList.removeClass('superbox-O');
+				$(this).addClass('superbox-O');
 
 				/*
 				 * Set sbImgBottom to bottom position of clicked image
 				 */
 				sbImgBottom = $(this).find('img').offset().top + $(this).find('img').height();
 
-				/*
-				 * Close any _other_ open superbox-show if it's not _this_ superbox-show.
-				 */
-				if (!$(this).next('.superbox-O').length) {
-					closeUp(sbList);
-				}
+				createSuperboxShow($(this));
+
+				keepShowAfterLast();
 
 				/*
-				 * Close _this_ superbox-show if it's open already, otherwise open it
+				 * Add source data to dynamically created full size image
 				 */
-				if ($(this).next('.superbox-O').length) {
-					/*
-					 * Reset sbImgBottom to 0 to trigger slide up
-					 */
-					sbImgBottom = 0;
-					closeUp($(this));
-				} else {
-					openUp($(this),imageData);
-				}
+				$('img.superbox-current-img')
+					.attr('src',$(this).find('img').data('img'));
+
+				openSuperboxShow();
 
 			});
 
